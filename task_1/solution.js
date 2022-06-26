a5358b26216ce3ee82232e667362a2014b2d5c9c
RKokarev <kokarev-roman@ya.ru>
2022-06-26 16:43:17 +0300

    task 1 is done

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,11 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+    let message = productName;
+    
+    message = `${message} за ${productPrice} теперь в корзине!`;
+    console.log (message);
+
     // Конец решения задания №1.1.
 
     return message;
@@ -27,6 +32,9 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+
+    let newValue = oldValue;
+    newValue = newValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -42,6 +50,9 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
